955e1756c569210fdadf384befcb68f41fa2f2ae
EricVartanov <82024638+EricVartanov@users.noreply.github.com>
2021-12-01 21:49:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,42 +2,97 @@
 
 function banner_resize() {
     var banners = [].slice.call(document.querySelectorAll('.idc-bg-wrapper'));
-    banners.forEach(function (elem) {
+    banners.forEach(function(elem) {
         return elem.style.width = document.documentElement.clientWidth + 'px';
     });
 }
 
-document.onreadystatechange = function () {
+// открытие/закрытие списка городов
+const list = document.querySelector('.idc-cities')
+const burger = document.querySelector('.idc-burger')
+
+function listOpen() {
+
+    list.addEventListener('click', function() {
+        burger.classList.toggle('open')
+    })
+
+    document.addEventListener('click', function(e) {
+        if (!list.contains(e.target)) {
+            burger.classList.remove('open')
+        }
+    })
+}
+
+const cityName = document.querySelectorAll('.idc-item-name')
+let allDots = document.querySelectorAll('.idc-dot')
+
+
+// функция, при наведении города сравнивает дата атр из списка и айди точки, при совпадении добавляет класс к тултипу точки 
+let tooltipOpen = function(names, dots) {
+    dots.forEach(function(dot) {
+        dot.addEventListener('mouseover', function() {
+            dot.classList.add('show')
+
+        })
+        dot.addEventListener('mouseout', function() {
+            if (!dot.getAttribute('data-clicked') == 1) {
+                dot.classList.remove('show')
+            }
+        })
+        dot.addEventListener('click', function() {
+
+            var dotName = dot.querySelector('.idc-tooltip')
+            let titleCity = document.querySelector('.idc-cities').firstChild
+            if (!dot.getAttribute('data-clicked') == 1) {
+                dots.forEach(function(e) {
+                    e.classList.remove('show')
+                    e.removeAttribute('data-clicked')
+
+                })
+                titleCity.textContent = dotName.textContent
+                dot.classList.add('show')
+                dot.setAttribute('data-clicked', 1)
+
+            }
+        })
+    })
+    names.forEach(function(name) {
+        name.addEventListener('mouseover', function() {
+            var elem = document.querySelector('#' + name.getAttribute('data-id') + '')
+            elem.classList.add('show')
+
+        })
+        name.addEventListener('mouseout', function() {
+            var elem = document.querySelector('#' + name.getAttribute('data-id') + '')
+            if (!elem.getAttribute('data-clicked') == 1) {
+                elem.classList.remove('show')
+            }
+
+        })
+        name.addEventListener('click', function() {
+            var elem = document.querySelector('#' + name.getAttribute('data-id') + '')
+            let titleCity = document.querySelector('.idc-cities').firstChild
+            allDots.forEach(function(e) {
+                e.classList.remove('show')
+                e.removeAttribute('data-clicked')
+            })
+            titleCity.textContent = name.textContent
+            elem.classList.add('show')
+            elem.setAttribute('data-clicked', 1)
+        })
+    })
+}
+
+document.onreadystatechange = function() {
     if (document.readyState === 'complete') {
         // Ваш скрипт
-        var AnchorsElem = [].slice.call(document.querySelectorAll("a[href^='#']")),
-            V = 0.5;
-        /* console.log(AnchorsElem) */
-        AnchorsElem.forEach(function (elem) {
-            elem.addEventListener('click', function (event) {
-                event.preventDefault()
-                var w = window.pageYOffset,  // прокрутка
-                    hash = this.href.replace(/[^#]*(.*)/, '$1'),  // id элемента, к которому нужно перейти
-                    t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
-                    start = null;
-                // подробнее про функцию анимации [developer.mozilla.org]
-                function step(time) {
-                    if (!start) start = time;
-                    var progress = time - start,
-                        r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
-                    window.scrollTo(0, r);
-                    if (r != w + t) {
-                        requestAnimationFrame(step)
-                    } else {
-                        location.hash = hash  // URL с хэшем
-                    }
-                }
-                window.requestAnimationFrame(step);
-                console.log(this)
-            }, false)
-        })
+        tooltipOpen(cityName, allDots)
 
-        window.addEventListener('resize', function () {
+        // открытие/закрытие списка городов
+        listOpen()
+
+        window.addEventListener('resize', function() {
             banner_resize();
         });
         banner_resize();
